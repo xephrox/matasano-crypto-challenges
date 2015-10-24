69e967790fdd8787b995b79bd16bc218b2e2e270
Sushil Shenoy <sushil.g.shenoy@gmail.com>
2015-10-24 22:26:13 +0530

    challenge 4 .. have to still work on optimizing challenge 3 and challenge 4

---
+++ Set 1/challenge4.py
@@ -0,0 +1,30 @@
+'''
+Created on Oct 24, 2015
+
+@author: Trinity
+'''
+
+import binascii
+import challenge3
+from Crypto.Util.strxor import strxor_c
+
+def decodefile(filename):
+    f = open(filename, 'r')
+    for line in f:
+        if line[-1] == '\n':
+            line = line[:-1]
+        s = binascii.unhexlify(line)
+        yield s
+
+def findXORLine(generator):
+    lines = [strxor_c(l, challenge3.answer(l)) for l in generator]
+    def compfunc(i):
+        return challenge3.englishness(lines[i])
+    maxI = max(range(len(lines)), key=compfunc)
+    return lines[maxI]
+if __name__ == '__main__':
+    print findXORLine(decodefile('extra/4.txt'))
+
+
+
+
